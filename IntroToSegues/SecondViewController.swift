51ba006d7cce06626d4733f551ed36de4f18abe1
paola-coder <70962088+paola-coder@users.noreply.github.com>
2021-07-29 12:09:32 -0400

    Add files via upload

---
+++ IntroToSegues/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  IntroToSegues
+//
+//  Created by Scholar on 7/29/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func button(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ IntroToSegues/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  IntroToSegues
+//
+//  Created by Scholar on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submitButttonTapped(_ sender: Any) {
+    }
+    
+}
+
